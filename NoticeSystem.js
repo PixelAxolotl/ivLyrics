5ba5e996194894e1617470de4abf8c94c0f963c2
ivLis <me@ivl.is>
2026-07-14 16:38:42 +0900

    6.0.3 RELEASE

--- NoticeSystem.js
+++ NoticeSystem.js
@@ -97,7 +97,7 @@ const getCurrentVersion = () => {
         window.Utils?.getCurrentVersion?.() ||
         window.CONFIG?.version ||
         window.ivLyricsVersion ||
-        "6.0.2"
+        "6.0.3"
     );
 };
 
--- Utils.js
+++ Utils.js
@@ -1341,7 +1341,7 @@ const Utils = {
   /**
    * Current version of the ivLyrics app
    */
-  currentVersion: "6.0.2",
+  currentVersion: "6.0.3",
 
   /**
    * Check for updates from remote repository
